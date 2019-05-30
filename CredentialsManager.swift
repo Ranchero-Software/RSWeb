cf3a30eb3833d9dd423fed003393e6e3c1a360d4
Jeremy Beker <gothmog@confusticate.com>
2019-05-30 07:47:33 -0400

    Retrieve and remove google auth credentials

--- CredentialsManager.swift
+++ CredentialsManager.swift
@@ -68,8 +68,52 @@ public struct CredentialsManager {
 		}
 		
 	}
-	
-	
+    
+    public static func retrieveGoogleAuthCredentials(server: String, username: String) throws -> Credentials? {
+        
+        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+                                    kSecAttrAccount as String: username,
+                                    kSecAttrServer as String: server,
+                                    kSecMatchLimit as String: kSecMatchLimitOne,
+                                    kSecReturnAttributes as String: true,
+                                    kSecReturnData as String: true]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        
+        guard status != errSecItemNotFound else {
+            return nil
+        }
+        
+        guard status == errSecSuccess else {
+            throw CredentialsError.unhandledError(status: status)
+        }
+        
+        guard let existingItem = item as? [String : Any],
+            let passwordData = existingItem[kSecValueData as String] as? Data,
+            let password = String(data: passwordData, encoding: String.Encoding.utf8) else {
+                return nil
+        }
+        
+        return Credentials.googleAuthLogin(username: username, apiKey: password)
+        
+    }
+    
+    public static func removeGoogleAuthCredentials(server: String, username: String) throws {
+        
+        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+                                    kSecAttrAccount as String: username,
+                                    kSecAttrServer as String: server,
+                                    kSecMatchLimit as String: kSecMatchLimitOne,
+                                    kSecReturnAttributes as String: true,
+                                    kSecReturnData as String: true]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw CredentialsError.unhandledError(status: status)
+        }
+        
+    }
 }
 
 // MARK: Private
