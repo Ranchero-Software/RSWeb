1ea5f5ccfc3646ffdf2891abbc5ea63e3d449def
Maurice Parker <mo@vincode.io>
2019-10-17 06:00:55 -0500

    Move extensions for compositing query items to RSWeb

--- RSWeb/URL+RSWeb.swift
+++ RSWeb/URL+RSWeb.swift
@@ -18,22 +18,18 @@ private struct URLConstants {
 public extension URL {
 	
 	func isHTTPSURL() -> Bool {
-		
 		return self.scheme?.lowercased() == URLConstants.schemeHTTPS
 	}
 	
 	func isHTTPURL() -> Bool {
-		
 		return self.scheme?.lowercased() == URLConstants.schemeHTTP
 	}
 	
 	func isHTTPOrHTTPSURL() -> Bool {
-		
 		return self.isHTTPSURL() || self.isHTTPURL()
 	}
 	
 	func absoluteStringWithHTTPOrHTTPSPrefixRemoved() -> String? {
-		
 		// Case-inensitive. Turns http://example.com/foo into example.com/foo
 		
 		if isHTTPSURL() {
@@ -45,12 +41,28 @@ public extension URL {
 		
 		return nil
 	}
+	
+	func appendingQueryItem(_ queryItem: URLQueryItem) -> URL? {
+		appendingQueryItems([queryItem])
+	}
+
+	func appendingQueryItems(_ queryItems: [URLQueryItem]) -> URL? {
+		guard var components = URLComponents(url: self, resolvingAgainstBaseURL: false) else {
+			return nil
+		}
+		
+		var newQueryItems = components.queryItems ?? []
+		newQueryItems.append(contentsOf: queryItems)
+		components.queryItems = newQueryItems
+		
+		return components.url
+	}
+	
 }
 
 private extension String {
 
 	func stringByRemovingCaseInsensitivePrefix(_ prefix: String) -> String {
-		
 		// Returns self if it doesn’t have the given prefix.
 		
 		let lowerPrefix = prefix.lowercased()
