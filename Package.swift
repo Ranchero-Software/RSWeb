470627488690f37052022a3a3d71c540f3c89858
Nate Weaver <wevah@derailer.org>
2020-07-31 15:54:16 -0500

    Remove empty dependencies entry

--- Package.swift
+++ Package.swift
@@ -22,7 +22,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RSWeb",
-            dependencies: [],
 			resources: [.copy("UTS46/uts46")],
 			swiftSettings: [.define("SWIFT_PACKAGE")]),
         .testTarget(
