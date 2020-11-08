6757efbe6273e68a0e33b78e02445c35c3899592
Maurice Parker <mo@vincode.io>
2020-11-08 12:19:34 -0600

    Added comments about why the enhanced percent encoding is needed

--- Sources/RSWeb/URLComponents+RSWeb.swift
+++ Sources/RSWeb/URLComponents+RSWeb.swift
@@ -9,6 +9,10 @@ import Foundation
 
 public extension URLComponents {
 	
+    	// `+` is a valid character in query component as per RFC 3986 (https://developer.apple.com/documentation/foundation/nsurlcomponents/1407752-queryitems)
+ 	// workaround:
+ 	// - http://www.openradar.me/24076063
+ 	// - https://stackoverflow.com/a/37314144
 	var enhancedPercentEncodedQuery: String? {
 		return percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B").replacingOccurrences(of: "%20", with: "+")
 	}
