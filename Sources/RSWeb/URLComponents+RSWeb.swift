2f9ad98736c5c17dfb2be0b3cc06e71a49b061fa
Maurice Parker <mo@vincode.io>
2021-03-15 14:46:16 -0500

    Change how we do percent encoding to allow for more special characters

--- Sources/RSWeb/URLComponents+RSWeb.swift
+++ Sources/RSWeb/URLComponents+RSWeb.swift
@@ -14,7 +14,21 @@ public extension URLComponents {
  	// - http://www.openradar.me/24076063
  	// - https://stackoverflow.com/a/37314144
 	var enhancedPercentEncodedQuery: String? {
-		return percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B").replacingOccurrences(of: "%20", with: "+")
+		guard !(queryItems?.isEmpty ?? true) else {
+			return nil
+		}
+		
+		var allowedCharacters = CharacterSet.urlQueryAllowed
+		allowedCharacters.remove(charactersIn: "!*'();:@&=+$,/?%#[]")
+		
+		var queries = [String]()
+		for queryItem in queryItems! {
+			if let value = queryItem.value?.addingPercentEncoding(withAllowedCharacters: allowedCharacters)?.replacingOccurrences(of: "%20", with: "+") {
+				queries.append("\(queryItem.name)=\(value)")
+			}
+		}
+		
+		return queries.joined(separator: "&")
 	}
 	
 }
